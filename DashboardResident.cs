e16cd9c75c948cf6312dc91a387f22379924c1ec
lenalee. <agcdeguzman24@bpsu.edu.ph>
2026-04-29 15:29:27 +0800

    Update DashboardResident.cs

--- DashboardResident.cs
+++ DashboardResident.cs
@@ -57,5 +57,70 @@ namespace Barangay_Management_System
         {
 
         }
+            private void label11_Click(object sender, EventArgs e)
+        {
+            txtResidentID.Text = "2026-001";
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+            label12.Text = "HH-1045";
+        }
+
+        private void label13_Click(object sender, EventArgs e)
+        {
+            label13.Text = "Juan Dela Cruz";
+        }
+
+        private void label14_Click(object sender, EventArgs e)
+        {
+            label14.Text = "09123456789";
+        }
+
+        private void label15_Click(object sender, EventArgs e)
+        {
+            label15.Text = "Balanga City,Bataan";
+        }
+
+        private void label31_Click(object sender, EventArgs e)
+        {
+            label31.Visible = false;
+        }
+
+        private void label32_Click(object sender, EventArgs e)
+        {
+            label32.Visible = false;
+        }
+
+        private void label33_Click(object sender, EventArgs e)
+        {
+            label33.Visible = false;
+        }
+
+        private void label34_Click(object sender, EventArgs e)
+        {
+            label34.Visible = false;
+        }
+
+        private void label37_Click(object sender, EventArgs e)
+        {
+            label37.Visible = false;
+        }
+
+        private void label38_Click(object sender, EventArgs e)
+        {
+            label38.Visible = false;
+        }
+
+        private void label36_Click(object sender, EventArgs e)
+        {
+            label15.Text = "Blotter Report #001\r\nDate: April 29, 2026\r\nComplainant: Maria Santos\r\nRespondent: Pedro Cruz\r\nIncident: Noise Complaint due to loud videoke at 11:30 PM\r\nStatus: Settled through barangay mediation.";
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
