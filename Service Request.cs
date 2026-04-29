617414b4a9893182de51affec84f879b53ef3c70
lenalee. <agcdeguzman24@bpsu.edu.ph>
2026-04-29 22:09:03 +0800

    Update Service Request.cs

--- Service Request.cs
+++ Service Request.cs
@@ -17,6 +17,38 @@ namespace Barangay_Management_System
             InitializeComponent();
         }
 
+               private void InsertRequest(string certificateType)
+        {
+            string connString = "server=localhost;user id=root;password=;database=barangay_management_system;";
+
+            using (MySqlConnection conn = new MySqlConnection(connString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    string query = "INSERT INTO certificate_requests (name, certificate, date_requested, status) " +
+                                   "VALUES (@name, @certificate, @date, @status)";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                    cmd.Parameters.AddWithValue("@name", "juan"); 
+                    cmd.Parameters.AddWithValue("@certificate", certificateType);
+                    cmd.Parameters.AddWithValue("@date", DateTime.Now.ToString("yyyy-MM-dd"));
+                    cmd.Parameters.AddWithValue("@status", "pending");
+
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show("Request Sent!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+
         private void button1_Click(object sender, EventArgs e)
         {
             DashboardResident f2 = new DashboardResident();
@@ -26,7 +58,40 @@ namespace Barangay_Management_System
 
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
+            
+        }
 
+       
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            InsertRequest("clearance");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            InsertRequest("indigency");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            InsertRequest("good moral");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            InsertRequest("residency");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            InsertRequest("solo parent");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            InsertRequest("no income");
         }
     }
 }
+
